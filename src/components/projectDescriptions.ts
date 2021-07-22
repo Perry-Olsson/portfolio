305f3c66ab1fa8f8a9926a0ce5e098eefb375d2a
Perry Olsson <olsson.perry@gmail.com>
2021-07-22 15:30:54 -0700

    start of layout

---
+++ src/components/projectDescriptions.ts
@@ -0,0 +1,9 @@
+import { html, TemplateResult } from "lit-html";
+
+export const OnlineBookingInfo = () => {
+  return html` <div>testing testing testing</div> `;
+};
+
+export const InfoModal = (component: TemplateResult) => {
+  return html` <div>${component}</div> `;
+};
