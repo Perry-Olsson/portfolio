482e6f04212346639aacf1e567cd21aeec81e73d
Perry Olsson <olsson.perry@gmail.com>
2025-10-05 20:22:24 -0700

    Update lighting

--- src/initScene/setLighting.ts
+++ src/initScene/setLighting.ts
@@ -2,30 +2,33 @@ import { AmbientLight, PointLight, Scene } from "three";
 
 const pointLightPosition = () => Math.ceil(window.innerWidth / 155);
 
-const pointLight1 = new PointLight(0xffffff);
-const pointLight2 = new PointLight(0xffffff);
-const pointLight3 = new PointLight(0xffffff);
-const pointLight4 = new PointLight(0xffffff);
+const pointLightColor = 0xaaaaaa
+const pointLight1 = new PointLight(pointLightColor);
+const pointLight2 = new PointLight(pointLightColor);
+const pointLight3 = new PointLight(pointLightColor);
+const pointLight4 = new PointLight(pointLightColor);
+
+const y = 2
 export const setLights = (scene: Scene) => {
-  pointLight1.position.set(0, 0, pointLightPosition());
+  pointLight1.position.set(0, y, pointLightPosition());
 
-  pointLight2.position.set(pointLightPosition(), 0, 0);
+  pointLight2.position.set(pointLightPosition(), y, 0);
 
-  pointLight3.position.set(pointLightPosition(), 0, 0);
+  pointLight3.position.set(pointLightPosition(), y, 0);
 
-  pointLight4.position.set(0, 0, pointLightPosition());
+  pointLight4.position.set(0, y, pointLightPosition());
 
-  const ambientLight = new AmbientLight(0xaaaaaa);
+  const ambientLight = new AmbientLight(pointLightColor);
 
   scene.add(ambientLight, pointLight1, pointLight2, pointLight3, pointLight4);
 };
 
 export const repositionLights = () => {
-  pointLight1.position.set(0, 0, pointLightPosition());
+  pointLight1.position.set(0, y, pointLightPosition());
 
-  pointLight2.position.set(pointLightPosition(), 0, 0);
+  pointLight2.position.set(pointLightPosition(), y, 0);
 
-  pointLight3.position.set(pointLightPosition(), 0, 0);
+  pointLight3.position.set(pointLightPosition(), y, 0);
 
-  pointLight4.position.set(0, 0, pointLightPosition());
+  pointLight4.position.set(0, y, pointLightPosition());
 };
