271666447ba1fe45fa3b3544a1d56792f89de8ba
Perry Olsson <olsson.perry@gmail.com>
2025-10-06 21:04:32 -0700

    Add orbital controls

--- src/initScene/initScene.ts
+++ src/initScene/initScene.ts
@@ -3,6 +3,7 @@ import { update } from "@tweenjs/tween.js";
 import { setLights } from "./setLighting";
 import { waitForLoad } from "./waitForLoad";
 import { Scene, WebGLRenderer, Color, Fog } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 const color = 0xbfbfbf
 
@@ -22,55 +23,18 @@ export const initScene = () => {
 
   waitForLoad();
 
-  addKeyListeners(camera)
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.listenToKeyEvents( window as any ); // optional
+  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+  controls.dampingFactor = 0.05;
+  controls.screenSpacePanning = false;
+  controls.maxPolarAngle = Math.PI / 2;
+
   function animate() {
     requestAnimationFrame(animate);
     update();
+    controls.update()
     renderer.render(scene, camera);
   }
   return { scene, renderer, camera, animate };
 };
-
-const movementDistance = 0.1;
-const addKeyListeners = (camera: Camera) => {
-    document.addEventListener('keydown', function(event) {
-        switch (event.key) {
-            case "ArrowRight":
-                if (event.ctrlKey) {
-                    camera.rotateY(-0.01)
-                } else {
-                    camera.position.setX(camera.position.x + movementDistance)
-                }
-                break;
-            case "ArrowLeft":
-                if (event.ctrlKey) {
-                    camera.rotateY(0.01)
-                } else {
-                    camera.position.setX(camera.position.x - movementDistance)
-                }
-                break;
-            case "ArrowUp":
-                if (event.ctrlKey) {
-                    camera.rotateX(0.01)
-                } else if (event.shiftKey) {
-                    camera.position.setY(camera.position.y + movementDistance)
-                } else {
-                    camera.position.setZ(camera.position.z - movementDistance)
-                }
-                break;
-            case "ArrowDown":
-                if (event.ctrlKey) {
-                    camera.rotateX(-0.01)
-                } else if (event.shiftKey) {
-                    camera.position.setY(camera.position.y - movementDistance)
-                } else {
-                    camera.position.setZ(camera.position.z + movementDistance)
-                }
-                break;
-        }
-        /* if (event.ctrlKey && event.key === 's') {
-            console.log('Ctrl + S pressed!');
-            event.preventDefault(); // Prevent default browser save action
-        } */
-    });
-}
